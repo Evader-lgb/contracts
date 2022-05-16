90cefd2cf13573ef835f58db6660b60e0afc3302
peng.zhi <zhipeng@66plat.com>
2022-05-16 18:53:21 +0800

    增加Greeter合约部署 Polygon

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
+
+const { ALCHEMY_API_URL, PRIVATE_KEY } = process.env;
+const mnemonic = PRIVATE_KEY
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -18,4 +22,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    bsc_testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: [`0x${PRIVATE_KEY}`]
+    },
+    mumbai: {
+      url: ALCHEMY_API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
 };
