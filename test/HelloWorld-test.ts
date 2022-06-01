d81f2928305199561ddef7ada6ad5936dab9a32c
peng.zhi <zhipeng@66plat.com>
2022-06-01 11:53:30 +0800

    增加输出合约字节码

--- test/HelloWorld-test.ts
+++ test/HelloWorld-test.ts
@@ -1,9 +1,22 @@
 import { expect } from "chai";
+import console from "console";
 import { ethers } from "hardhat";
 
 describe("Contract HelloWorld", function () {
   it("测试HelloWorld", async function () {
     const HelloWorld = await ethers.getContractFactory("HelloWorld");
+    console.log(
+      "HelloWorld byteCode keccak256:",
+      ethers.utils.keccak256(HelloWorld.bytecode)
+    );
+    console.log(
+      "HelloWorld byteCode id:",
+      ethers.utils.id(HelloWorld.bytecode)
+    );
+    console.log(
+      "HelloWorld byteCode id:",
+      ethers.utils.keccak256(ethers.utils.toUtf8Bytes(HelloWorld.bytecode))
+    );
     const helloworld = await HelloWorld.deploy("I'm tester!");
     await helloworld.deployed();
 
