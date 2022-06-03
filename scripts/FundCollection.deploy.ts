61289fc64ab3e59bdb0b014dd3897bdd9e26dc11
peng.zhi <zhipeng@66plat.com>
2022-06-03 09:16:12 +0800

    重构

--- scripts/FundCollection.deploy.ts
+++ scripts/FundCollection.deploy.ts
@@ -4,26 +4,11 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 // const hre = require("hardhat");
-import hre, { upgrades } from "hardhat";
 import { deployUpgradeProxy, deployUpgradeUpdate } from "./utils/deploy.util";
 
-async function firstDeploy() {
-  // We get the contract to deploy
-  const DeployContractName = "FundCollection";
-  const DeployContract = await hre.ethers.getContractFactory(
-    DeployContractName
-  );
-  const deployContract = await upgrades.deployProxy(DeployContract);
-
-  await deployContract.deployed();
-
-  console.log("%s deployed to: %s", DeployContractName, deployContract.address);
-  return deployContract.address;
-}
-
 async function main() {
-  const contractAddress = null;
-  //   const contractAddress = "0x33E7FB51160499a6D009BD504E25cc827BfB5e28";
+  //   const contractAddress = null;
+  const contractAddress = "0x33E7FB51160499a6D009BD504E25cc827BfB5e28";
   if (contractAddress) {
     await deployUpgradeUpdate("FundCollection", contractAddress);
   } else {
