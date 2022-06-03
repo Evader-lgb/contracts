496b4e22fb2513313fcccd9501686ac2323a57cd
peng.zhi <zhipeng@66plat.com>
2022-06-03 09:13:08 +0800

    增加补充普通合约的助手函数

--- scripts/HelloWorld-deploy.ts
+++ scripts/HelloWorld-deploy.ts
@@ -1,33 +1,8 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-import hre from "hardhat";
-
-async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-
-  // We get the contract to deploy
-  const DeployContractName = "HelloWorld";
-  const DeployContract = await hre.ethers.getContractFactory(
-    DeployContractName
-  );
-  const deployContract = await DeployContract.deploy("Hello, Macondo!");
-
-  await deployContract.deployed();
-
-  console.log("%s deployed to:%s", DeployContractName, deployContract.address);
-}
+import { deployNormal } from "./utils/deploy.util";
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main()
+deployNormal("HelloWorld", "Hello, Macondo!")
   .then(() => process.exit(0))
   .catch((error) => {
     console.error(error);
--- scripts/utils/deploy.util.ts
+++ scripts/utils/deploy.util.ts
@@ -30,7 +30,21 @@ async function _deploy(DeployContractName: string, deployContract: Contract) {
   return deployContract.address;
 }
 /**
- * 部署普通合约
+ * 部署普通合约(不可升级)
+ * @param DeployContractName  contract name
+ * @param args  contract args
+ * @returns  contract address
+ */
+export async function deployNormal(DeployContractName: string, ...args: any[]) {
+  const DeployContract = await hre.ethers.getContractFactory(
+    DeployContractName
+  );
+  const deployContract = await DeployContract.deploy(...args);
+  return _deploy(DeployContractName, deployContract);
+}
+
+/**
+ * 部署可升级合约
  * @param contractName  合约名称
  * @returns  合约地址
  */
@@ -41,37 +55,13 @@ export async function deployUpgradeProxy(contractName: string) {
   );
   const deployContract = await upgrades.deployProxy(DeployContract);
   return _deploy(DeployContractName, deployContract);
-
-  console.log("[deploy contract]:deploy [%s] start", DeployContractName);
-  const [deployer] = await hre.ethers.getSigners();
-  console.log("[deploy contract]:deployer address", deployer.address);
-  const deployerBalance = await deployer.getBalance();
-  console.log(
-    "[deploy contract]:deployer balance before",
-    deployerBalance.toString()
-  );
-  await deployContract.deployed();
-  console.log(
-    "[deploy contract]:%s upgrade to: %s",
-    DeployContractName,
-    deployContract.address
-  );
-
-  const deployerBalanceAfter = await deployer.getBalance();
-  console.log(
-    "[deploy contract]:deployer balance after",
-    deployerBalanceAfter.toString()
-  );
-  console.log(
-    "[deploy contract]:deploy gas fee",
-    deployerBalance.sub(deployerBalanceAfter).toString()
-  );
-  console.log(
-    "[deploy contract]:deploy contract: [%s] complete",
-    DeployContractName
-  );
-  return deployContract.address;
 }
+/**
+ * 更新可升级合约
+ * @param contractName 合约名称
+ * @param contractAddress  合约地址
+ * @returns
+ */
 export async function deployUpgradeUpdate(
   contractName: string,
   contractAddress: string
