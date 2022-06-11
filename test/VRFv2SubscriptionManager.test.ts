699fb15d2983c7a69fc0b036ef321ae4b157acb8
peng.zhi <zhipeng@66plat.com>
2022-06-11 10:56:33 +0800

    重构测试用例

--- test/VRFv2SubscriptionManager.test.ts
+++ test/VRFv2SubscriptionManager.test.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { BigNumber, Contract, Wallet } from "ethers";
+import { BigNumber, Contract } from "ethers";
 import { ethers } from "hardhat";
 import { ERC20TokenUtil } from "./utils/ERC20Token.util";
 // 3.指定合约的合约地址
@@ -7,30 +7,29 @@ const contractAddress = "0xeABe0c63EBef2F49ec93d31369c9F1C552532C62";
 // 4.指定合约调用地址和默认调用私钥
 const { ALCHEMY_API_TESTNET_URL, PRIVATE_KEY } = process.env;
 
-const ERC20ABI = require("../third_party_abi/TokenERC20.json");
-
-function createERC20Contract(erc20Address: string, signer?: Wallet): Contract {
-  return new ethers.Contract(erc20Address, ERC20ABI, signer);
-}
-function createERCLink(signer?: Wallet): Contract {
-  return new ethers.Contract(
-    "0x326C977E6efc84E512bB9C30f76E30c160eD06FB",
-    ERC20ABI,
-    signer
-  );
-}
-
 describe("VRFv2SubscriptionManager", function () {
-  it("VRFv2SubscriptionManager Test", async function () {
+  let vRFv2SubscriptionManager: Contract;
+  let provider: any;
+  beforeEach(async function () {
     const VRFv2SubscriptionManager = await ethers.getContractFactory(
       "VRFv2SubscriptionManager"
     );
-    const vRFv2SubscriptionManager = await VRFv2SubscriptionManager.attach(
+    vRFv2SubscriptionManager = await VRFv2SubscriptionManager.attach(
       contractAddress
     );
-    const provider = new ethers.providers.JsonRpcProvider(
-      ALCHEMY_API_TESTNET_URL
-    );
+    provider = new ethers.providers.JsonRpcProvider(ALCHEMY_API_TESTNET_URL);
+  });
+
+  it.only("VRFv2SubscriptionManager Test", async function () {
+    // const VRFv2SubscriptionManager = await ethers.getContractFactory(
+    //   "VRFv2SubscriptionManager"
+    // );
+    // const vRFv2SubscriptionManager = await VRFv2SubscriptionManager.attach(
+    //   contractAddress
+    // );
+    // const provider = new ethers.providers.JsonRpcProvider(
+    //   ALCHEMY_API_TESTNET_URL
+    // );
 
     const signer = new ethers.Wallet(PRIVATE_KEY as string, provider);
     const contract = await vRFv2SubscriptionManager.connect(signer);
