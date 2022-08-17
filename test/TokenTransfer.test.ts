430f19cd7612cf17e2a1d9cd617b688672a296e5
peng.zhi <zhipeng@66plat.com>
2022-08-17 18:17:19 +0800

    修复测试用例BUG

--- test/TokenTransfer.test.ts
+++ test/TokenTransfer.test.ts
@@ -1,62 +1,77 @@
-import { expect } from "chai";
-import { ethers } from "hardhat";
+import { expect } from 'chai';
+import { Contract } from 'ethers';
+import { ethers } from 'hardhat';
 
 const { ALCHEMY_API_MAINNET_URL, PRIVATE_KEY } = process.env;
-describe("TokenTransfer", function () {
-  it("TokenTransfer Test", async function () {
-    const TokenTransfer = await ethers.getContractFactory("TokenTransfer");
-    const tokenTransfer = await TokenTransfer.deploy(
-      "0x4Ea9C43e09A51ba39a31a29f2b7494F60C766E66"
-    );
+describe('TokenTransfer', function () {
+  let macondoUSDT: Contract;
+  let macondoUSDTAddress: string;
+  beforeEach(async function () {
+    const MacondoUSDT = await ethers.getContractFactory('MacondoUSDT');
+    macondoUSDT = await MacondoUSDT.deploy();
+    const macondoUSDTDeploy = await macondoUSDT.deployed();
+    macondoUSDTAddress = macondoUSDTDeploy.address;
+  });
+
+  it('TokenTransfer Test', async function () {
+    const TokenTransfer = await ethers.getContractFactory('TokenTransfer');
+    const tokenTransfer = await TokenTransfer.deploy(macondoUSDTAddress);
     const address = await (await tokenTransfer.deployed()).address;
-    console.log(address);
+    console.log(`address: ${address}`);
     const balanceBefore = await tokenTransfer.getContractBalance();
-    console.log("balanceBefore", balanceBefore);
-    expect(balanceBefore).to.equal("0");
+    expect(balanceBefore).to.equal('0');
 
     const USDTContract = await ethers.getContractAt(
-      "MacondoUSDT",
-      "0x4Ea9C43e09A51ba39a31a29f2b7494F60C766E66"
+      'MacondoUSDT',
+      macondoUSDTAddress
     );
 
     const [owner, addr1] = await ethers.getSigners();
     const signer = new ethers.Wallet(PRIVATE_KEY as string, owner.provider);
+    //transfer signer gas fee
+    await owner.sendTransaction({
+      to: signer.address,
+      value: ethers.utils.parseEther('5'),
+    });
+    //transfer signer 5 usdt
+    await macondoUSDT.transfer(signer.address, ethers.utils.parseEther('5'));
+
     const result = await USDTContract.connect(signer).approve(
       tokenTransfer.address,
-      "1000000000000000000"
+      ethers.utils.parseEther('1')
     );
 
-    await expect(tokenTransfer.connect(signer).deposit("1000000000000000000"))
-      .to.emit(tokenTransfer, "tokenChangedEvent")
-      .withArgs("1000000000000000000");
+    await expect(
+      tokenTransfer.connect(signer).deposit(ethers.utils.parseEther('1'))
+    )
+      .to.emit(tokenTransfer, 'tokenChangedEvent')
+      .withArgs(ethers.utils.parseEther('1'));
     const balanceAfter = await tokenTransfer.getContractBalance();
 
-    console.log("balanceAfter", balanceAfter);
-    expect(balanceAfter).to.equal("1000000000000000000");
+    console.log('balanceAfter', balanceAfter);
+    expect(balanceAfter).to.equal(ethers.utils.parseEther('1'));
 
     // check counter
     const counter = await tokenTransfer.counter();
-    console.log("counter", counter);
+    console.log('counter', counter);
   });
 
-  it("TokenTransfer Transfer USDT Test", async function () {
-    const TokenTransfer = await ethers.getContractFactory("TokenTransfer");
-    const tokenTransfer = await TokenTransfer.deploy(
-      "0x4Ea9C43e09A51ba39a31a29f2b7494F60C766E66"
-    );
+  it('TokenTransfer Transfer USDT Test', async function () {
+    const TokenTransfer = await ethers.getContractFactory('TokenTransfer');
+    const tokenTransfer = await TokenTransfer.deploy(macondoUSDTAddress);
     const address = await (await tokenTransfer.deployed()).address;
     //before transfer into contract balance
     const balanceBeforeTransferIntoContract =
       await tokenTransfer.getContractBalance();
     console.log(
-      "before transfer into contract balance:",
+      'before transfer into contract balance:',
       balanceBeforeTransferIntoContract
     );
-    expect(balanceBeforeTransferIntoContract).to.equal("0");
+    expect(balanceBeforeTransferIntoContract).to.equal('0');
 
     const USDTContract = await ethers.getContractAt(
-      "MacondoUSDT",
-      "0x4Ea9C43e09A51ba39a31a29f2b7494F60C766E66"
+      'MacondoUSDT',
+      macondoUSDTAddress
     );
 
     const [owner, addr1] = await ethers.getSigners();
@@ -64,35 +79,35 @@ describe("TokenTransfer", function () {
 
     //provider on transfer event
     const filter = {
-      address: "0x4Ea9C43e09A51ba39a31a29f2b7494F60C766E66",
+      address: macondoUSDTAddress,
       topics: [
         // the name of the event, parnetheses containing the data type of each event, no spaces
-        ethers.utils.id("Transfer(address,address,uint256)"),
+        ethers.utils.id('Transfer(address,address,uint256)'),
       ],
     };
     signer.provider.on(filter, (result) => {
-      console.log("result", result);
+      console.log('result', result);
     });
 
     //before transfer
     const balanceBefore = await USDTContract.balanceOf(signer.address);
-    console.log("before transfer:", balanceBefore);
+    console.log('before transfer:', balanceBefore);
     const result = await USDTContract.connect(signer).transfer(
       tokenTransfer.address,
-      "100"
+      '100'
     );
     result.wait();
 
     const balanceAfter = await USDTContract.balanceOf(signer.address);
-    console.log("after transfer:", balanceAfter);
+    console.log('after transfer:', balanceAfter);
 
     //after transfer into contract balance
     const balanceAfterTransferIntoContract =
       await tokenTransfer.getContractBalance();
     console.log(
-      "after transfer into contract balance:",
+      'after transfer into contract balance:',
       balanceAfterTransferIntoContract
     );
-    expect(balanceAfterTransferIntoContract).to.equal("100");
+    expect(balanceAfterTransferIntoContract).to.equal('100');
   });
 });
