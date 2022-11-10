aa93758d9b46d687968b3fdc948ba2015622280b
peng.zhi <zhipeng@66plat.com>
2022-11-10 18:21:44 +0800

    增加线上随机数

--- test/tests/core/utils/random/OnChainRandomUpgradeableTest.test.ts
+++ test/tests/core/utils/random/OnChainRandomUpgradeableTest.test.ts
@@ -23,4 +23,20 @@ describe('OnChainRandomUpgradeableTest', () => {
 
     expect(random3).not.to.equal(random2);
   });
+
+  it('should return a random number with a seed', async () => {
+    const seed =
+      '46672134033264489063721065857950797364633243009474115366978012516340856789160';
+    const random1 = await contract.getRandomBySeed(seed, 0);
+    const random2 = await contract.getRandomBySeed(seed, 0);
+
+    expect(random1).to.equal(
+      '78186544682697568394469585502892849925345949349572456956614040161341429022826'
+    );
+    expect(random1).to.equal(random2);
+
+    const random3 = await contract.getRandomBySeed(seed, 1);
+
+    expect(random3).not.to.equal(random2);
+  });
 });
